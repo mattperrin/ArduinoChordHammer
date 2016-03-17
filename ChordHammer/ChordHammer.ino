0bf23d1ee979983a816cd7be1ba77ac3882a5b16
Matthew Perrin <mattperrin@gmail.com>
2016-03-16 22:37:03 -0400

    ok, adding midi

--- ChordHammer/ChordHammer.ino
+++ ChordHammer/ChordHammer.ino
@@ -9,7 +9,8 @@
 * LCD BL pin to digital pin 10
 * KEY pin to analog pin 0        (14)  THIS IS THE BUTTON PIN
 */
-
+#include <MIDI.h>
+#include <Wire.h>
 #include <LiquidCrystal.h>
 
 LiquidCrystal lcd(8, 13, 9, 4, 5, 6, 7);
@@ -23,30 +24,40 @@ LiquidCrystal lcd(8, 13, 9, 4, 5, 6, 7);
 int adc_key_val[5] ={50, 200, 400, 600, 800 };
 int NUM_KEYS = 5;*/
 int adc_key_in;
+unsigned long randomKey1_1;
+
+
+
+MIDI_CREATE_DEFAULT_INSTANCE();
 
 void setup()
 {
-  pinMode(A0, INPUT);
+  randomSeed(4321);
+ // pinMode(A0, INPUT);
+
+    MIDI.begin();
+  Serial.begin(115200);
   
-  Serial.begin(9600);
+  //ffuuuuuuu Serial.begin(9600);
   lcd.clear(); 
   lcd.begin(16, 2);
   lcd.setCursor(0,0); 
-  lcd.print("ADC key testing"); 
+  lcd.print("Chord Crusher v1"); 
 
   delay(2000);
 }
 
 void loop()
 {
-  counter += 1;
+  //                      4294967295
+  randomKey1_1 = random(0,999999999);
   
   lcd.clear();
   lcd.setCursor(0,0);
-  lcd.print("Looping..."); 
+  lcd.print((String)randomKey1_1); 
   adc_key_in = analogRead(0);
   lcd.setCursor(0,1);
-  lcd.print((String)counter + " " + (String)adc_key_in);
+  lcd.print((String)adc_key_in);
 
-  //delay(200);
+  delay(1000);
 }
